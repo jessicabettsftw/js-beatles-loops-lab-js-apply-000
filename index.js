8f92ccb444d7e16d9eebf0d79332dc83933e3d8e
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2018-11-12 01:13:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function theBeatlesPlay(musicians, instruments) {
   var temp = [];
-  for(i = 0; i ++; i < musicians.length();){
-    String member = musicians[i];
-    String instrument = instruments[i];
-    String combo = member + " plays " + instrument;
+  for(i = 0; i ++; i < musicians.length){
+    String member = musicians[i]
+    String instrument = instruments[i]
+    String combo = member + " plays " + instrument
     temp[i] = combo;
   }
 }
